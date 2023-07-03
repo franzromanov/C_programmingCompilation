db4a95caa54be3d6f2ed43df591ac9a90c52cc1b
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-03 22:52:47 +0800

    Update inside_lib.c

--- Array&Pointer/InsideLib/inside_lib.c
+++ Array&Pointer/InsideLib/inside_lib.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 #include "inside.h"
 int inside(int val,int *arr,int len){
+//integer
+for(int i =0; i<len;i++){
+if(val ==*(arr+i)){return 1; break;}
+}
+
+return 0;
+}
+//floating point
+float inside(float val,float *arr,int len){
 
 for(int i =0; i<len;i++){
 if(val ==*(arr+i)){return 1; break;}
