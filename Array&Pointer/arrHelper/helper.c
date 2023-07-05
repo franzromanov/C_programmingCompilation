ec91a2dc2795479b68bfff2154df425c86adb61f
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-05 22:44:58 +0800

    Update helper.c

--- Array&Pointer/arrHelper/helper.c
+++ Array&Pointer/arrHelper/helper.c
@@ -2,19 +2,26 @@
 #include "helper.h"
 
 //integer
+//FillArray
 void arrFill(int *arr,int len){
-
 for(int i=0;i<len;i++){
 printf("input data#%d: ",i+1);
 scanf("%d",arr+i);
 }
 
 }
-
+//PrintArray
 void arrPrint(int *arr,int len){
 printf("[");
 for(int i=0;i<len-1;i++){
 printf("%d, ",*(arr+i));
 }
 printf("%d]\n",*(arr+len-1));
+}
+
+//sum of all elements
+void SeriesVal(int *arr,int len,int *bag){
+int sum=0;
+for(int i=0; i<len;i++)sum=sum+(*(arr+i));
+*bag=sum;
 }
