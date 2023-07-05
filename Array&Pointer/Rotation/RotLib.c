be4490ac55cd1dfbc00434b1d0179ab9f7367300
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-05 13:14:08 +0800

    Update RotLib.c

--- Array&Pointer/Rotation/RotLib.c
+++ Array&Pointer/Rotation/RotLib.c
@@ -2,23 +2,36 @@
 #include "rotate.h"
 #include "helper.h"
 
+//IntegerType
+
+//LeftRotation
 void rotateLeft(int *arr,int len, int pos){
 int tmp[len];
 int catch;
-//rotate_1pos:
-
-//starting_point
+//mainProcess
 for(int i=0;i<len;i++)tmp[i]=0;
-
 for(int i=0; i<len; i++){
 catch=i-pos;
 if((catch)<0)tmp[len+catch]=*(arr+i);
 if(catch>=0)tmp[catch]=*(arr+i);
-
 }
-
+//result
 for(int i=0;i<len;i++){
 *(arr+i)=tmp[i];
 }
+}
 
+//RightRotation
+void rotateRight(int *arr, int len, int pos){
+int tmp[len];
+int catch;
+//mainprocess
+for(int i=0;i<len;i++)tmp[i]=0;
+for(int i=0;i<len;i++){
+catch=i+pos;
+if(catch>(len-1))tmp[catch-len]=*(arr+i);
+if(catch<=(len-1))tmp[catch]=*(arr+i);
+}
+//result
+for(int i=0;i<len;i++)*(arr+i)=tmp[i];
 }
