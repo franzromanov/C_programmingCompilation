584f6bc841fe14c5e258723c6a5387a236571238
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-07 09:25:14 +0800

    Update stringext.c

--- Strings/stringext/stringext.c
+++ Strings/stringext/stringext.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "stringext.h"
 //word_counter
 int wordcount(char* str,int len){
 int counter=0;
@@ -9,4 +9,18 @@ if(*(str+i)>=0x41&&*(str+i)<=0x5A||*(str+i)>=0x61&&*(str+i)<=0x7A)counter=counte
 }
 
 return counter;
+}
+
+//palindrome_checker
+int strPalindrome(char *str,int len){
+//process
+char dump[len-1];
+char tmp[len-1];
+for(int i=0;i<len-1;i++){
+dump[i]=*(str+i);
+tmp[len-1-1-i]=*(str+i);
+}
+//result
+if(strcmp(dump,tmp)==0)return 1;
+else return 0;
 }
