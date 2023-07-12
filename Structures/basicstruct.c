93c51c8065bb71ccc1b3915a5f2f704adb7dcf80
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-12 14:25:04 +0800

    Add files via upload

---
+++ Structures/basicstruct.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//defineStruct
+struct basic{
+int number;
+int def;
+};
+
+//decare
+struct basic num;
+
+//main
+//inp
+int main(){
+num.number=1;
+scanf("%d",&num.def);
+
+//out
+printf("%d>>>>%d\n",num.def,num.number);
+
+
+//esc
+return 0;
+}
