d8320f4c38ace1388bf41b183e818a24483fb350
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-20 12:45:30 +0800

    Add files via upload

---
+++ Array&Pointer/counting_array/upper.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//declare
+char dump[]={'K','I','B','R','C','K','Z','M'};
+int min,max,i,frq,hld;
+char val;
+
+//main
+int main(){
+ max=dump[0];
+ min=dump[0];
+//buffer
+ for(i=0;i<8;i++){
+  if(max<dump[i])max=dump[i];
+  if(min>dump[i])min=dump[i];
+}
+
+//count
+ hld=(max-min)+1;
+ int count[hld];
+ //regulate
+ for(i=0;i<hld;i++)count[i]=0;
+ for(i=0;i<8;i++)count[(int)dump[i]-(int)min]++;
+
+ //end_product
+ frq=count[0];
+ for(i=0;i<(int)hld;i++){if(frq<count[i]){frq=count[i];val=(char)i+(char)min;}}
+ printf("[frq] %d :: [val] %c\n",frq,val);
+//esc
+return 0;
+}
