fdcd72b19a579954992de2b3bb4bbe933cca1e7f
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-07-28 18:47:49 +0800

    Create asteriskPattern.c

---
+++ asteriskPattern.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+printf("1*********\n");
+printf("12********\n");
+printf("123*******\n");
+printf("1234******\n");
+printf("1235******\n");
+
+//esc
+ return 0;
+}
